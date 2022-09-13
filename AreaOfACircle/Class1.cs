392d23c6fbfcda190e54915f788da4b25dc80c63
A.J. Miller <ajmiller1206@gmail.com>
2022-09-13 13:05:48 -0400

    first commit

---
+++ AreaOfACircle/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class Class1
+{
+	public static double AreaOfCircle(double radius)
+	{
+		double area = Math.PI * Math.Pow(radius, 2);
+		return area;
+	}
+}
---
+++ AreaOfACircle/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Input the radius of a circle: ");
+double radius = double.Parse(Console.ReadLine());
+Console.WriteLine($"The area of your circle is {Class1.AreaOfCircle(radius)}");
+Console.ReadLine();
